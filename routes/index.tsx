7aef60c6f14e112de150db4a2484e701c45d9577
ravil yakupov <monofactura@icloud.com>
2025-11-11 11:01:49 +0300

    Add favicon to app and remove logo from Home component

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,13 +20,6 @@ export default define.page(function Home() {
       <div class="relative w-screen h-screen overflow-hidden">
         <P5Hero />
         <div class="relative z-10 w-full h-full flex flex-col items-center justify-center gap-6 px-4">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
           <h1 class="text-4xl font-bold text-white drop-shadow-lg">Welcome to fresh-v2-shadcn-boilerplate</h1>
         </div>
       </div>
